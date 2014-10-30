69c950164c45ead4a1ff89af81bc497d398f6864
Michael Truog <mjtruog@gmail.com>
2014-10-30 16:36:17 -0700

    Update hex package.

---
+++ mix.exs
@@ -0,0 +1,30 @@
+defmodule CloudIServiceApiRequests do
+  use Mix.Project
+
+  def project do
+    [app: :cloudi_service_api_requests,
+     version: "1.4.0-rc.4",
+     description: description,
+     package: package,
+     deps: deps]
+  end
+
+  defp deps do
+    [{:cloudi_core, "~> 1.4.0-rc.4"},
+     {:trie, "~> 1.4.0-rc.4"},
+     {:jsx, "~> 2.1.1"}]
+  end
+
+  defp description do
+    "Erlang/Elixir Cloud Framework API requests (JSON-RPC/Erlang-term support)"
+  end
+
+  defp package do
+    [files: ~w(src doc rebar.config README.markdown),
+     contributors: ["Michael Truog"],
+     licenses: ["BSD"],
+     links: %{"Website" => "http://cloudi.org",
+              "GitHub" => "https://github.com/CloudI/" <>
+                          "cloudi_service_api_requests"}]
+   end
+end
