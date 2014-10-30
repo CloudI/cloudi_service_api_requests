73d11a2bd4fb84c9219d7026cf96834085f8fed3
Michael Truog <mjtruog@gmail.com>
2014-10-30 16:40:14 -0700

    Tweak hex package.

--- mix.exs
+++ mix.exs
@@ -4,6 +4,7 @@ defmodule CloudIServiceApiRequests do
   def project do
     [app: :cloudi_service_api_requests,
      version: "1.4.0-rc.4",
+     language: :erlang,
      description: description,
      package: package,
      deps: deps]
