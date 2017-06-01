034bbc0eb086af3f4d490743545604b09899825b
Michael Truog <mjtruog@gmail.com>
2017-05-31 18:36:16 -0700

    Change license from BSD-4-Clause to MIT

--- mix.exs
+++ mix.exs
@@ -26,7 +26,7 @@ defmodule CloudIServiceApiRequests do
   defp package do
     [files: ~w(src doc rebar.config README.markdown),
      maintainers: ["Michael Truog"],
-     licenses: ["BSD"],
+     licenses: ["MIT"],
      links: %{"Website" => "http://cloudi.org",
               "GitHub" => "https://github.com/CloudI/" <>
                           "cloudi_service_api_requests"}]
